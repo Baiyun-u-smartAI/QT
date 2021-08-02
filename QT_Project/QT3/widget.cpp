7ef7ce1c4b99341f952921cc5d3326cc46b5b8a4
Baiyun-u-smartAI <45277409+Baiyun-u-smartAI@users.noreply.github.com>
2021-08-02 20:51:19 +0800

    无边框倒计时进度条

---
+++ QT_Project/QT3/widget.cpp
@@ -0,0 +1,34 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    this->setWindowFlags(Qt::FramelessWindowHint|Qt::WindowStaysOnTopHint);
+    this->setWindowOpacity(1);
+    this->setAttribute(Qt::WA_TranslucentBackground);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+//鼠标按下
+void Widget::mousePressEvent(QMouseEvent *e)
+{
+
+    last = e->globalPos();
+}
+
+//鼠标移动
+void Widget::mouseMoveEvent(QMouseEvent *e)
+{
+
+    int dx = e->globalX() - last.x();
+    int dy = e->globalY() - last.y();
+    last = e->globalPos();
+    move(x()+dx,y()+dy);
+}
